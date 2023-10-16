4ebe50df841498eb5b1490fa17f26b5bfca13fc2
Saurav Das <sauravdas969.sd@gmail.com>
2023-10-16 19:41:20 +0530

    Creating App Service Plan

---
+++ 3_tier_application/c3_appserviceplan.tf
@@ -0,0 +1,25 @@
+# Check the following document for more information on how to configure app service plan
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/service_plan
+# Front-End App Service Plan
+resource "azurerm_service_plan" "fe-asp" {
+  name                = "fe-asp-prod"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  os_type             = "Linux"
+  sku_name            = "B1"
+  depends_on = [
+    azurerm_subnet.fe-subnet
+  ]
+}
+
+# Backend App Service Plan
+resource "azurerm_service_plan" "be-asp" {
+  name                = "be-asp-prod"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  os_type             = "Linux"
+  sku_name            = "B1"
+  depends_on = [
+    azurerm_subnet.be-subnet
+  ]
+}
