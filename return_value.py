baf37aae94d8e689083e3c43e2943bc34cb22d95
Saurav Das <sauravdas969.sd@gmail.com>
2023-10-17 00:52:35 +0530

    Returning value of key from nested object

---
+++ return_value.py
@@ -0,0 +1,25 @@
+# First we will initialize the dictionary and the key
+obj = {"x":{"y":{"z":"a"}}}
+key = "x/y/z"
+
+def return_value(obj1, key1):
+    keys = key1.split('/') # We are making a list of keys using split method which split string into a list
+    value = obj1
+
+    # Now we will itirate in the list of keys to get value for each key until the last key
+    for i in keys:
+        # checking if the key (i) is present in the dictionary
+        if i in value:
+            value = value[i] # substituting the value for the key
+        else:
+            return None
+    
+    # Returning final value
+    return value
+
+
+# Checking the result by calling the function we created above
+result = return_value(obj, key)
+
+# Printing result
+print(f"Expected value for the following key '{key}' is '{result}'")
