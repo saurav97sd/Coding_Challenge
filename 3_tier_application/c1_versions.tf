4b89bfc04c84140171df3934a6c4d0d105ecee59
Saurav Das <sauravdas969.sd@gmail.com>
2023-10-16 19:37:50 +0530

    Configuring Azure Provider and terraform block

---
+++ 3_tier_application/c1_versions.tf
@@ -0,0 +1,26 @@
+# For more informatio check the following documentation
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs
+# Azure Provider source and version being used
+terraform {
+  required_version = "~> 1.5.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "rg" {
+  name     = "3-tier-app-rg" # Required
+  location = var.location-rg # Required
+  tags = {
+    "Application" = "3tier_app"
+  }
+}
